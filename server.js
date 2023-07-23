47e6cf26c2a7299ece9b0dc7514ad457fe3d6c45
Omkar4033 <91587106+Omkar4033@users.noreply.github.com>
2023-07-23 14:47:02 +0530

    added gitignore

--- server.js
+++ server.js
@@ -2,13 +2,16 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const striperoutes=require('./Routes/stripe-routes');
-// Set up API routes
 const pizzaRouter = require('./Routes/Pizza');
 const orderRouter = require('./Routes/Order');
 const userRouter =require('./Routes/User')
 const cors =require('cors');
 const app = express();
-const port = process.env.PORT || 5000;
+require('dotenv').config();
+
+
+const port = process.env.PORT;
+const mongourl=process.env.MONGODB_URI;
 
 // Set up body-parser middleware
  app.use(bodyParser.json());
@@ -17,9 +20,8 @@ const port = process.env.PORT || 5000;
     res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
   });
 
-// Connect to MongoDB database
 
-const mongourl="mongodb+srv://raghuomkar9604022654:Omkar4033@cluster0.nfd9det.mongodb.net/pizzaPlanet?retryWrites=true&w=majority"
+
 mongoose.set('strictQuery', false);
 mongoose.connect(mongourl, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => console.log('MongoDB connected'))
   .catch((err) => console.log(err));
